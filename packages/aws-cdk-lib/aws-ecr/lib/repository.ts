e43f4086cf71ad8e3f1ecc92a7c120015dc5b20d
EC2 Default User <ec2-user@ip-10-0-0-43.ap-northeast-1.compute.internal>
2024-07-06 12:38:00 +0000

    modify

--- packages/aws-cdk-lib/aws-ecr/lib/repository.ts
+++ packages/aws-cdk-lib/aws-ecr/lib/repository.ts
@@ -491,6 +491,20 @@ export interface OnImageScanCompletedOptions extends events.OnEventOptions {
   readonly imageTags?: string[];
 }
 
+/**
+ * The tag mutability setting for your repository.
+ */
+export enum ImageTagMutability {
+  /**
+   * allow image tags to be overwritten.
+   */
+  MUTABLE = 'MUTABLE',
+  /**
+   * all image tags within the repository will be immutable which will prevent them from being overwritten.
+   */
+  IMMUTABLE = 'IMMUTABLE'
+}
+
 export interface RepositoryProps {
   /**
    * Name for this repository.
@@ -570,6 +584,16 @@ export interface RepositoryProps {
    * @default false
    */
   readonly emptyOnDelete?: boolean;
+
+    /**
+   * The tag mutability setting for the repository.
+   *
+   * If this parameter is omitted, the default setting of MUTABLE will be used which will allow image tags to be overwritten.
+   * If IMMUTABLE is specified, all image tags within the repository will be immutable which will prevent them from being overwritten.
+   *
+   *  @default ImageTagMutability.MUTABLE
+   */
+    readonly imageTagMutability?: ImageTagMutability;
 }
 
 export interface RepositoryAttributes {
@@ -707,6 +731,7 @@ export class Repository extends RepositoryBase {
       imageScanningConfiguration: props.imageScanOnPush !== undefined ? { scanOnPush: props.imageScanOnPush } : undefined,
       encryptionConfiguration: this.parseEncryption(props),
       emptyOnDelete: props.emptyOnDelete,
+      imageTagMutability: props.imageTagMutability,
     });
     this._resource = resource;
 
